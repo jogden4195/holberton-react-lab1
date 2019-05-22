52f9b1c04ae40fcceec985eb0c51828283bbe4a3
Jenn <jennogden95@gmail.com>
2019-05-21 23:29:03 -0700

    Deleted demo files; Adding title, todo-app, and add-todo

---
+++ src/title.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Title extends Component {
+  render() {
+    return <title>Lets get lootin' for this gluten</title>;
+  }
+}
+
+export default Title;
